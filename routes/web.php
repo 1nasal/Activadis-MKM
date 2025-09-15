acb915965a5f74068f88dee84fa0a404605fca16
Milan Kuster <milan.kuster@student.graafschapcollege.nl>
2025-09-15 13:46:06 +0200

    routes fixed

--- routes/web.php
+++ routes/web.php
@@ -3,28 +3,37 @@
 use App\Http\Controllers\ActivityController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ActivityController;
+
+// Home page - shows activities using activityList.blade.php
+Route::get('/', [ActivityController::class, 'home'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-//Profile
+
+// Profile routes
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-//Activities
-Route::middleware('auth')->group(function () {
-    Route::resource('activities', ActivityController::class);
+
+// Activity routes
+Route::prefix('activities')->name('activities.')->group(function () {
+    // Public index route - uses activity.index view
+    Route::get('/', [ActivityController::class, 'index'])->name('index');
+    
+    // Protected routes (auth required)
+    Route::middleware('auth')->group(function () {
+        Route::get('/create', [ActivityController::class, 'create'])->name('create');
+        Route::post('/', [ActivityController::class, 'store'])->name('store');
+        Route::get('/{activity}/edit', [ActivityController::class, 'edit'])->name('edit');
+        Route::patch('/{activity}', [ActivityController::class, 'update'])->name('update');
+        Route::delete('/{activity}', [ActivityController::class, 'destroy'])->name('destroy');
+    });
+    
+    // Show route comes last (dynamic parameter)
+    Route::get('/{activity}', [ActivityController::class, 'show'])->name('show');
 });
-
-Route::get('/activity', function () {
-    return view('activity');
-})->name('activity');
-
-Route::get('/', [App\Http\Controllers\ActivityController::class, 'index']);
-Route::get('/activities', [App\Http\Controllers\ActivityController::class, 'index'])->name('activities.index');
-Route::get('/activities/{activity}', [App\Http\Controllers\ActivityController::class, 'show'])->name('activities.show');
 
 require __DIR__.'/auth.php';
